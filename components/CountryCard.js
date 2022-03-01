f42ce1dedacd2241e81f8b6bfee614ac66403b78
pauline-wim <linette.wim@hotmail.com>
2022-03-01 21:10:10 +0100

    css

--- components/CountryCard.js
+++ components/CountryCard.js
@@ -2,28 +2,31 @@ import { StyleSheet, View, Text } from "react-native";
 
 export default function CountryCard(props) {
   return (
-    <View style={styles.country}>
+    <View style={styles.country} index={props.index}>
       <View style={styles.countryFlag}>
         <Text style={styles.countryName}>{props.name}</Text>
         <Text>{props.flag}</Text>
       </View>
-      <Text>{props.capital}</Text>
+      <Text style={styles.capital}>{props.capital}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  list: {
-    padding: 10,
-  },
   country: {
-    marginBottom: 20,
+    height: 200,
+    textAlign: "center",
+    paddingTop: 60,
   },
   countryFlag: {
     display: "inline",
   },
   countryName: {
+    fontSize: 20,
     fontWeight: "bold",
     paddingRight: 10,
+  },
+  capital: {
+    fontSize: 20,
   },
 });
